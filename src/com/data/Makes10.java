3fc1b2294caff193228fb40d1da226c19506d716
ZennT <zenn.diamond@gmail.com>
2021-03-18 08:27:36 -0700

    added Makes10.java

---
+++ src/com/data/Makes10.java
@@ -0,0 +1,17 @@
+package com.data;
+
+/**
+ * Given 2 ints, a and b,
+ * return true if one if them is 10 or if their sum is 10.
+ */
+
+public class Makes10 {
+
+    public static void main(String[] args) {
+        makes10(9,10);
+    }
+
+    }
+    public static boolean makes10(int a, int b) {
+
+    }
