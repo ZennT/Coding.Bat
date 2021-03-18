059bbdb0d9f9b702362683764f6bc0783cca7c56
ZennT <zenn.diamond@gmail.com>
2021-03-18 08:59:36 -0700

    added NearHundred.java

---
+++ src/com/data/NearHundred.java
@@ -0,0 +1,20 @@
+package com.data;
+
+/**
+ * Given an int n,
+ * return true if it is within 10 of 100 or 200.
+ * Note: Math.abs(num) computes the absolute value of a number.
+ */
+
+public class NearHundred {
+
+    public static void main(String[] args) {
+        nearHundred(247);
+
+    }
+
+    public static boolean nearHundred(int n) {
+
+    }
+
+}
