62b23a2f3b2d80381bd272b486a095d2aeb141ac
ZennT <zenn.diamond@gmail.com>
2021-03-17 23:01:06 -0700

    update on MonkeyTrouble.java

--- src/com/data/MonkeyTrouble.java
+++ src/com/data/MonkeyTrouble.java
@@ -1,4 +1,15 @@
 package com.data;
 
 public class MonkeyTrouble {
+
+    public static void main(String[] args) {
+        monkeyTrouble(true,true);
+    }
+    public static boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+
+        if(aSmile && bSmile || !aSmile && !bSmile){
+            return true;
+        }
+        return false;
+    }
 }
