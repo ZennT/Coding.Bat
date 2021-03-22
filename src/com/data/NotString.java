19770b643eb19d7209d0bd09ef4fb044688971a5
ZennT <zenn.diamond@gmail.com>
2021-03-22 01:31:14 -0700

    adden NotString.java

---
+++ src/com/data/NotString.java
@@ -0,0 +1,13 @@
+package com.data;
+
+/**
+ * Given a string, return a new string where "not "
+ * has been added to the front. However,
+ * if the string already begins with "not", return the string unchanged.
+ * Note: use .equals() to compare 2 strings.
+ */
+
+public class NotString {
+
+
+}
